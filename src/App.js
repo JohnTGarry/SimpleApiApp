038b1ecee083299bb2e29e5d3c3948535124da47
RivalGarry <johntgarry@gmail.com>
2021-10-03 22:55:46 +0100

    PokemonItem and prettier

--- src/App.js
+++ src/App.js
@@ -1,48 +1,52 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import './App.css'
-import PageNavigator from './PageNavigator'
-import PokemonList from './PokemonList'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import PageNavigator from "./PageNavigator";
+import PokemonList from "./PokemonList";
 
 // GET https://pokeapi.co/api/v2/pokemon/{id or name}/
 
 export default function App() {
-  const [pokemonList, setPokemonList] = useState([])
-  const [currentPageUrl, setCurrentPageUrl] = useState('https://pokeapi.co/api/v2/pokemon')
-  const [prevPageUrl, setPrevPageUrl] = useState('')
-  const [nextPageUrl, setNextPageUrl] = useState('')
-  const [isLoading, setIsLoading] = useState(true)
+  const [pokemonList, setPokemonList] = useState([]);
+  const [currentPageUrl, setCurrentPageUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon"
+  );
+  const [prevPageUrl, setPrevPageUrl] = useState("");
+  const [nextPageUrl, setNextPageUrl] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    setIsLoading(true)
-    let cancel
-    axios.get(currentPageUrl,
-      { cancelToken: new axios.CancelToken(c => cancel = c) })
-      .then(res => {
-        setIsLoading(false)
-        setPokemonList(res.data.results)
-        setPrevPageUrl(res.data.previous)
-        setNextPageUrl(res.data.next)
+    setIsLoading(true);
+    let cancel;
+    axios
+      .get(currentPageUrl, {
+        cancelToken: new axios.CancelToken((c) => (cancel = c)),
       })
-      .catch(err => console.error(err))
+      .then((res) => {
+        setIsLoading(false);
+        setPokemonList(res.data.results);
+        setPrevPageUrl(res.data.previous);
+        setNextPageUrl(res.data.next);
+      })
+      .catch((err) => console.error(err));
 
     return () => {
-      cancel()
-    }
-  }, [currentPageUrl])
+      cancel();
+    };
+  }, [currentPageUrl]);
 
   const fetchPrevPage = () => {
-    setCurrentPageUrl(prevPageUrl)
-  }
+    setCurrentPageUrl(prevPageUrl);
+  };
 
   const fetchNextPage = () => {
-    setCurrentPageUrl(nextPageUrl)
-  }
+    setCurrentPageUrl(nextPageUrl);
+  };
 
-  if (isLoading) return <p>{'Loading...'}</p>
+  if (isLoading) return <p>{"Loading..."}</p>;
 
   return (
-    <div className='App'>
+    <div className="App">
       <PokemonList pokemonList={pokemonList} />
       <PageNavigator
         fetchPrevPage={fetchPrevPage}
